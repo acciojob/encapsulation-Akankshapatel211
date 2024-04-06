ee1b413d65cbac52942c328a6a7218b704f4c17b
Akankshapatel211 <akt1292002@gmail.com>
2024-04-06 17:17:01 +0530

    Modified main.java and RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class RWOnly {
+	
+	private  int Id;
+	private String Name ;
+	
+    public void  SetRWOnly(int Id){
+	  	this.Id=Id;
+	}
+    public void  SetRWOnly(String Name){
+	  	this.Name=Name;
+	}
+    public int GetRWOnly() {
+    	return Id;
+    }
+    public String getRWOnly() {
+
+    	return Name;
+    }
     
 }
